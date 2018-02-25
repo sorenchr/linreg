b96dd1219693c5a385a00c44a17a68473f77beb9
Søren Qvist Christensen <soren.qvist@gmail.com>
2018-02-25 11:23:03 +0100

    Setup main module to load a CSV file into an array

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
-from numpy import matrix
-import csv
 import argparse
+import numpy
+import csv
 
 
-def test():
-    print('test')
+def run(datafile):
+    dataarray = readcsv(datafile)
+    matrix = numpy.matrix(dataarray)
+    print(matrix)
+
+
+def readcsv(file):
+    """Read a CSV file into a multidimensional array of rows and columns."""
+    rows = []
+    with open(file, newline='') as csvfile:
+        reader = csv .reader(csvfile, delimiter=',', quotechar='"')
+        next(reader, None)  # headers
+        for row in reader:
+            rows.append([float(x) for x in row])
+    return rows
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("data", type=str, help="the CSV file containing the data")
     args = parser.parse_args()
-    test()
+    run(args.data)
